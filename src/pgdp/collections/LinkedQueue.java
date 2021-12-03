63bcab94c15dbe1878b53c018dc1e839093ae7b6
kiu_khalatiani.nikolai <khalatiani.nikolai@kiu.edu.ge>
2021-12-03 16:59:09 +0400

    no message

---
+++ src/pgdp/collections/LinkedQueue.java
@@ -0,0 +1,23 @@
+package pgdp.collections;
+
+public class LinkedQueue implements Queue{
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Object dequeue() {
+        return null;
+    }
+
+    @Override
+    public void enqueue(Object thing) {
+
+    }
+}
---
+++ src/pgdp/collections/LinkedStack.java
@@ -0,0 +1,24 @@
+package pgdp.collections;
+
+public class LinkedStack implements Stack{
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public void push(Object thing) {
+
+    }
+}
