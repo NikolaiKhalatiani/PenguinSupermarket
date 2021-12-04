71ec7c6046312609eb3f561aa4c66302ad147e05
kiu_khalatiani.nikolai <khalatiani.nikolai@kiu.edu.ge>
2021-12-04 15:49:01 +0400

    no message

--- src/pgdp/collections/QueueConnector.java
+++ src/pgdp/collections/QueueConnector.java
@@ -1,18 +1,23 @@
 package pgdp.collections;
 
 public class QueueConnector<T> implements DataStructureConnector<T>{
+    private Queue<T> myQueue = new LinkedQueue<>();
+    public QueueConnector(Queue<T> myQueue){
+        this.myQueue=myQueue;
+    }
     @Override
     public T removeNextElement() {
-        return null;
+        if(myQueue.isEmpty()) return null;
+        else return myQueue.dequeue();
     }
 
     @Override
     public void addElement(T thing) {
-
+        myQueue.enqueue(thing);
     }
 
     @Override
     public boolean hasNextElement() {
-        return false;
+        return !myQueue.isEmpty();
     }
 }
