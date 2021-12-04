b63d82c2a86a3ac6850667071d27ecb5e18984ef
kiu_khalatiani.nikolai <khalatiani.nikolai@kiu.edu.ge>
2021-12-04 18:59:21 +0400

    no message

---
+++ src/pgdp/collections/FishyProduct.java
@@ -0,0 +1,28 @@
+package pgdp.collections;
+
+public class FishyProduct {
+    private String name;
+    private int price;
+
+    public FishyProduct(final String name, final int price) {
+        if (name != null && price > 0) {
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "new FishyProduct("+'"' + name + '"'+
+                ", " + price +
+                ')';
+    }
+}
