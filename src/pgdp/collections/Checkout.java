38b49413028c208a02d02362bab65e8718032481
kiu_khalatiani.nikolai <khalatiani.nikolai@kiu.edu.ge>
2021-12-06 21:06:00 +0400

    no message

---
+++ src/pgdp/collections/Checkout.java
@@ -0,0 +1,45 @@
+package pgdp.collections;
+
+public class Checkout {
+    private final Queue<PenguinCustomer> queue;
+    private final Queue<FishyProduct> bandBeforeCashier;
+    private final Queue<FishyProduct> bandAfterCashier;
+
+
+    public Checkout() {
+        this.queue = new LinkedQueue<>();
+        this.bandBeforeCashier = new LinkedQueue<>();
+        this.bandAfterCashier = new LinkedQueue<>();
+    }
+
+    public Queue<PenguinCustomer> getQueue() {
+        return queue;
+    }
+
+    public Queue<FishyProduct> getBandBeforeCashier() {
+        return bandBeforeCashier;
+    }
+
+    public Queue<FishyProduct> getBandAfterCashier() {
+        return bandAfterCashier;
+    }
+
+    public int queueLength() {
+        return queue.size();
+    }
+
+    public void serveNextCustomer() {
+        if (!queue.isEmpty()) {
+            PenguinCustomer pengi = queue.dequeue();
+            pengi.placeAllProductsOnBand(bandBeforeCashier);
+            int sum = 0;
+            for (int check = 0; check <= bandBeforeCashier.size() ; check++) {
+                FishyProduct item = bandBeforeCashier.dequeue();
+                sum+=item.getPrice();
+                bandAfterCashier.enqueue(item);
+            }
+            pengi.pay(sum);
+            pengi.takeAllProductsFromBand(bandAfterCashier);
+        }
+    }
+}
