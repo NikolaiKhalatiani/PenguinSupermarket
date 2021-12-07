2957cb41e19832c651046e0179823b73e31550d3
kiu_khalatiani.nikolai <khalatiani.nikolai@kiu.edu.ge>
2021-12-07 21:59:31 +0400

    no message

--- src/pgdp/collections/PenguinSupermarket.java
+++ src/pgdp/collections/PenguinSupermarket.java
@@ -18,7 +18,7 @@ public class PenguinSupermarket {
     }
 
     public Checkout getCheckoutWithSmallestQueue() {
-        int firstLine, secondLine, smallestLine = 0, realSmallest = checkouts[0].queueLength();
+        int firstLine, secondLine, smallestLine,realSmallest = checkouts[0].queueLength();
         Checkout smallest = new Checkout();
         for (int index = 0; index < checkouts.length - 1; index++) {
             for (int jndex = 1; jndex < checkouts.length; jndex++) {
@@ -38,7 +38,26 @@ public class PenguinSupermarket {
     }
 
     public void closeCheckout(int index) {
+        if (index >= checkouts.length || checkouts.length == 0 || checkouts.length == 1 || index < 0)
+            ExceptionUtil.illegalArgument("This is not Allowed");
+        else {
+            Checkout closedCheckout = checkouts[index];
+            Checkout[] arrayOfCheckouts = new Checkout[checkouts.length - 1];
+            for (int jndex = 0; jndex < checkouts.length; jndex++) {
+                if (jndex > index) arrayOfCheckouts[jndex - 1] = checkouts[jndex];
+                if (jndex < index) arrayOfCheckouts[jndex] = checkouts[jndex];
+            }
+            checkouts = arrayOfCheckouts;
+            if (closedCheckout.queueLength() == 0) return;
+            DataStructureConnector<PenguinCustomer> closedCheckout1 = new QueueConnector<>(closedCheckout.getQueue());
+            DataStructureConnector<PenguinCustomer> needThisStackToArrangeThemBackwards = new StackConnector<>(new LinkedStack<>());
+            DataStructureLink<PenguinCustomer> goGoGo = new DataStructureLink<>(closedCheckout1, needThisStackToArrangeThemBackwards);
+            goGoGo.moveAllFromAToB();
+            while (needThisStackToArrangeThemBackwards.hasNextElement())
+                needThisStackToArrangeThemBackwards.removeNextElement().goToCheckout(this);
+        }
     }
+
     public void serveCustomers() {
         for (Checkout checkout : checkouts) checkout.serveNextCustomer();
     }
